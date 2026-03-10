884e5baf74d089fa625bdb4c6b43ca12ff26d5d6
p-gupta98 <pihugupta0501@gmail.com>
2026-03-10 16:44:11 -0700

    Create two svg circles on top of each other and use strokeDashOffset to create a moving circle effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ export default function App() {
   return (
     <div>
       <h1>Boxing Timer</h1>
+      <svg height="700" width="700">
+        <circle r="200" cx="300" cy="300" fill="none" stroke="black" strokeWidth="15" opacity="0.5"/> 
+        <circle r="200" cx="300" cy="300" fill="none" stroke="red" strokeWidth="10" strokeLinecap="round" strokeDasharray="1257" strokeDashoffset="100" transform="rotate(-80, 300, 300)"/> 
+      </svg>
     </div>
   )
 }
