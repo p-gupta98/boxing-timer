6c72df050cb45694b8da99f83ff2a3a724d2226d
p-gupta98 <pihugupta0501@gmail.com>
2026-03-11 15:24:04 -0700

    Add viewBox to center circle and switch hard coded values with dynamic variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 export default function App() {
-  const radius = 200;
+  const size = 600;
+  const centerViewBox = size / 2;
+  const radius = centerViewBox / 3;
   const circumference = 2 * Math.PI * radius;
   const progress = 0.75;
   const offset = circumference * (1 - progress);
+  const cy = centerViewBox - 100;
+  
   return (
     <div>
       <h1>Boxing Timer</h1>
-      <svg height="700" width="700">
-        <circle r={radius} cx="300" cy="300" fill="none" stroke="black" strokeWidth="15" opacity="0.5"/> 
-        <circle r={radius} cx="300" cy="300" fill="none" stroke="red" strokeWidth="10" strokeLinecap="round" strokeDasharray={circumference} strokeDashoffset={offset} transform="rotate(-90, 300, 300)"/> 
+      <svg viewBox ={`0 0 ${size} ${size}`} >
+        <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="black" strokeWidth="15" opacity="0.5"/> 
+        <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="red" strokeWidth="10" strokeLinecap="round" strokeDasharray={circumference} strokeDashoffset={offset} 
+        transform={`rotate(-90, ${centerViewBox}, ${cy})`}/> 
       </svg>
     </div>
   )
