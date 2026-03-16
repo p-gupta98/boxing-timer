767a96468f2af3a42cceb7a90016b9890fc9fa12
p-gupta98 <pihugupta0501@gmail.com>
2026-03-15 21:04:17 -0700

    Add start, pause, resume buttons. Add isRunning state which determines when the timer runs and stops

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,46 @@ import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
 
-  const [timeLeft, setTimeLeft] = useState(10);
+  const setTimerSeconds = 10;
+
+  const [timeLeft, setTimeLeft] = useState(setTimerSeconds);
+  const [isRunning, setIsRunning] = useState(false);
   const ref = useRef();
 
+  function start() {
+    setIsRunning(true);
+  }
+
+  function pause() {
+    setIsRunning(false);
+  }
+
+  function reset() {
+    setIsRunning(false);
+    // reset the timer
+    setTimeLeft(setTimerSeconds);
+  }
+
   useEffect(() => {
-    ref.current = setInterval(() => {
+    if (isRunning) {
+      clearInterval(ref.current)
+      ref.current = setInterval(() => {
       setTimeLeft((prev) => {
         if (prev === 0) {
           clearInterval(ref.current);
           return 0;
         }
         return prev - 1});
-    }, 1000);
+      }, 1000);
 
-    // Cleanup 
-    return () => {
-      clearInterval(ref.current);
+      // Cleanup 
+      return () => {
+        clearInterval(ref.current);
+      }
     }
+  }, [isRunning]);
 
-  }, []);
+  
 
   const size = 600;
   const centerViewBox = size / 2;
@@ -34,6 +55,9 @@ export default function App() {
   return (
     <div>
       <h1>Boxing Timer</h1>
+      <button type="button" onClick={start}>Start</button>
+      <button type="button" onClick={pause}>Pause</button>
+      <button type="reset" onClick={reset}>Reset</button>
       <svg viewBox ={`0 0 ${size} ${size}`} >
         <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="black" strokeWidth="15" opacity="0.5"/> 
         <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="red" strokeWidth="10" strokeLinecap="round" strokeDasharray={circumference} strokeDashoffset={offset} 
