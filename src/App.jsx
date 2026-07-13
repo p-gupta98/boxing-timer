28ecea2902ee5c334195f273b847fc2d59e895d6
p-gupta98 <pihugupta0501@gmail.com>
2026-07-13 11:41:07 -0700

    Replace setInterval with dispatch function calls to state. Replace all use of useState with state object

--- src/App.jsx
+++ src/App.jsx
@@ -70,8 +70,6 @@ export default function App() {
   const [timeLeft, setTimeLeft] = useState(fightDuration);
   const [isRunning, setIsRunning] = useState(false);
   const ref = useRef();
-  const phaseRef = useRef("idle");
-  const roundRef = useRef(1);
   const bellRef = useRef(null);
 
   function playBell() {
@@ -97,12 +95,10 @@ export default function App() {
 
   function start() {
     setIsRunning(true);
-    setTimerPhase("fight");
-    phaseRef.current = "fight";
+    dispatch({type: "START"});
     if (!bellRef.current) {
       bellRef.current = new AudioContext();
     }
-    
     playBell();
   }
 
@@ -112,62 +108,15 @@ export default function App() {
 
   function reset() {
     setIsRunning(false);
-    // reset the timer
-    setTimeLeft(fightDuration);
-
-    // reset the round to 1
-    setCurrentRound(1);
-    roundRef.current = 1;
-
-    // reset the phase to idle
-    setTimerPhase("idle");
-    phaseRef.current = "idle";
+    // Dispatch sets all the states in one
+    dispatch({type: "RESET"});
   }
 
   useEffect(() => {
     if (isRunning) {
       clearInterval(ref.current)
       ref.current = setInterval(() => {
-      setTimeLeft((prev) => {
-        if (prev === 1) {
-          console.log("transitioning from:", phaseRef.current, "time:", Date.now());
-          // When the last round's fight timer has ended
-          if (roundRef.current === totalRounds && phaseRef.current == "fight") {
-            clearInterval(ref.current);
-            // Move to the done phase - reset timer completely
-            setTimerPhase("done");
-            phaseRef.current = "done";
-            // play bell sounds thrice when all rounds are done
-            playBell();
-            setTimeout(() => playBell(), 300);
-            setTimeout(() => playBell(), 600);
-            reset();
-          }
-          // If it's still not the last round
-          else if (phaseRef.current === "fight") {
-            // Move to the rest phase
-            setTimerPhase("rest");
-            phaseRef.current = "rest";
-            setCurrentRound(prev => prev + 1);
-            roundRef.current = roundRef.current + 1;
-            // PLay bell sound twice when fight phase finishes
-            playBell();
-            setTimeout(() => playBell(), 300);
-            // Set the timer to rest duration
-            return restDuration;
-          }
-          // If the timer is in the rest phase
-          else if (phaseRef.current === "rest") {
-            // Set it to fight phase
-            setTimerPhase("fight");
-            phaseRef.current = "fight";
-            // Play bell sound once when fight phase starts
-            playBell();
-            return fightDuration;
-          }
-          return 0;
-        }
-        return prev - 1});
+        dispatch({type: "TICK"});
       }, 1000);
 
       // Cleanup 
@@ -183,8 +132,8 @@ export default function App() {
   const centerViewBox = size / 2;
   const radius = centerViewBox / 3;
   const circumference = 2 * Math.PI * radius;
-  const totalDuration = timerPhase === "rest" ? restDuration : fightDuration;
-  const progress = timeLeft / totalDuration;
+  const totalDuration = state.phase === "rest" ? restDuration : fightDuration;
+  const progress = state.timeLeft / totalDuration;
   const offset = circumference * (1 - progress);
   const cy = centerViewBox - 100;
   
@@ -198,9 +147,9 @@ export default function App() {
         <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="black" strokeWidth="15" opacity="0.5"/> 
         <circle r={radius} cx={centerViewBox} cy={cy} fill="none" stroke="red" strokeWidth="10" strokeLinecap="round" strokeDasharray={circumference} strokeDashoffset={offset} 
         transform={`rotate(-90, ${centerViewBox}, ${cy})`}/> 
-        <text x={centerViewBox} y={cy} textAnchor="middle">{timeLeft}</text>
-        <text x ={centerViewBox} y={cy + 30} textAnchor='middle'>Round {currentRound}/{totalRounds}</text>
-        <text x ={centerViewBox} y={cy + 50} textAnchor='middle'>{timerPhase}</text>
+        <text x={centerViewBox} y={cy} textAnchor="middle">{state.timeLeft}</text>
+        <text x ={centerViewBox} y={cy + 30} textAnchor='middle'>Round {state.round}/{totalRounds}</text>
+        <text x ={centerViewBox} y={cy + 50} textAnchor='middle'>{state.phase}</text>
       </svg>
     </div>
   )
